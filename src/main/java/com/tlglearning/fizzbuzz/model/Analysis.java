2a25af1723c9d45fc3c9c0f3023eb2f225a48b10
blworth <worthambrandon@gmail.com>
2022-07-27 14:44:08 -0500

    Implemented parameterized tests w/ spec change.

--- src/main/java/com/tlglearning/fizzbuzz/model/Analysis.java
+++ src/main/java/com/tlglearning/fizzbuzz/model/Analysis.java
@@ -5,11 +5,25 @@ import java.util.Set;
 
 public class Analysis {
 
+  public static final String ILLEGAL_VALUE_FORMAT = "Value was %,d; must be non-negative";
+
   //below this method will return a set of the ate FIZZ, BUZZ in the State tab... now we are going to implement
   //the return type is what is before Set<State>, so set, we are returning a set of state
   //we are returning a set containing both fizz and buzz value in it or neither of them
   //first we declare a local variable then return it
-  public Set<State> analyze(int value) {
+
+  /**
+   *
+   * @param value
+   * @return
+   * @throws IllegalArgumentException if {@code value < 0}.
+   */
+  public Set<State> analyze(int value) throws IllegalArgumentException {
+
+    if (value <0) {
+      throw new IllegalArgumentException(String.format(ILLEGAL_VALUE_FORMAT, value));
+    }
+
     Set<State> result = EnumSet.noneOf(State.class);    //
 
     if (value % 3 == 0) {          //%3 mean remainder of 3 for divisible, now saying if the number is divisible by 3 with no remainders it will reply w/FIZZ
---
+++ src/test/java/com/tlglearning/fizzbuzz/model/AnalysisTest.java
@@ -0,0 +1,69 @@
+package com.tlglearning.fizzbuzz.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.EnumSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class AnalysisTest {
+  static final Set<State> fizzexpected = EnumSet.of(State.FIZZ);
+  static final Set<State> neitherexpected = EnumSet.noneOf(State.class);
+
+ final Analysis analysis = new Analysis();
+
+  //or
+
+//  Analysis analysis = new Analysis();
+//
+//  @BeforeEach
+//  void setup() {
+//    analysis = new Analysis();
+//  }
+
+
+
+
+  @ParameterizedTest
+  @ValueSource(ints = {3, 999_999_999})
+  void analyze_fizz(int value) {
+    assertEquals(fizzexpected, analysis.analyze(value));    //can do it like this with fields above or can do the one below with no fields
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {5, 85, 999_999_985})
+  void analyze_buzz(int value) {
+    Set<State> expected = EnumSet.of(State.BUZZ);
+    assertEquals(expected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {0, 15, 999_999_990})
+  void analyze_fizzBuzz(int value) {
+    Set<State> expected = EnumSet.of(State.FIZZ, State.BUZZ);
+    assertEquals(expected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "neither.csv", numLinesToSkip = 1)
+  void analyze_neither(int value) {
+    assertEquals(neitherexpected, analysis.analyze(value));
+  }
+
+  @ParameterizedTest
+  @ValueSource(ints = {-1, -3, -5, -15})
+  void analyze_negative(int value) {
+    try {
+      analysis.analyze(value);
+      fail();
+    } catch (IllegalArgumentException e) {
+      // Do nothing; this is the expected behavior.
+    }
+  }
+
+}
